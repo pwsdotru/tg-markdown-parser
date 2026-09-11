6bd923f1e5084a8b0af55cecee78a668603c9382
Aleksandr Novikov <pwsdotru@gmail.com>
2026-09-11 23:05:06 +0300

    Examples script

--- example.php
+++ example.php
@@ -2,24 +2,32 @@
 
 declare(strict_types=1);
 
+echo("Start\n");
+
 global $entities;
 
 require_once(__DIR__ . "/vendor/autoload.php");
 
 use TgMarkdownParser\Parser;
 
-$example = "00";
+if (array_key_exists(1, $argv)) {
+    $example = trim($argv[1]);
+} else {
+    $example = "00";
+}
 
 $ent = sprintf(__DIR__ . "/examples/example%s.php", $example);
-
+echo("Including file to entities: " . $ent . "\n");
 require_once($ent);
 
 $text = sprintf(__DIR__ . "/examples/example%s.txt", $example);
 
+echo("Reading plain text: " . $text . "n");
 $parser = new Parser(file_get_contents($text), $entities);
 
+echo("Parsing\n");
 $parser->parse();
 
-echo("Done:\n");
+echo("\nDone\nResult:\n---\n");
 echo($parser->getMarkdown());
-echo("\n");
+echo("\n---\n");
