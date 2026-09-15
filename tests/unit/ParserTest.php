6c081c22daaf2cbdb84a686a3706963eb6fd8c66
Aleksandr Novikov <pwsdotru@gmail.com>
2026-09-15 23:46:25 +0300

    Added tests

--- tests/unit/ParserTest.php
+++ tests/unit/ParserTest.php
@@ -6,6 +6,7 @@ namespace unit;
 
 use TgMarkdownParser\Parser;
 use PHPUnit\Framework\TestCase;
+use PHPUnit\Framework\Attributes\DataProvider;
 
 final class ParserTest extends TestCase
 {
@@ -20,5 +21,37 @@ final class ParserTest extends TestCase
         $text = "Test text";
         $obj = new Parser($text);
         $this->assertEquals($text, $obj->getText());
+    }
+
+    /**
+     * @return array<array<mixed>, array<mixed>>
+     */
+    public static function sortEntitiesProvider(): array
+    {
+        return [
+            [
+                [],
+                [],
+            ],
+            [
+                [
+                    ['offset' => 20, 'length' => 10, 'type' => 'bold'],
+                ],
+                [
+                    ['offset' => 20, 'length' => 10, 'type' => 'bold'],
+                ]
+            ],
+        ];
+    }
+
+    /**
+     * @param array<array<mixed>> $input
+     * @param array<array<mixed>> $expected
+     */
+    #[DataProvider('sortEntitiesProvider')]
+    public function testSortEntities(array $input, array $expected): void
+    {
+        $obj = new Parser("", $input);
+        $this->assertEquals($expected, $obj->getEntities());
     }
 }
